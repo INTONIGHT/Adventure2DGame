96d3160483f0e6e3b85a032fad5d45fb22e7c72a
INTONIGHT <tyler.throop@gmail.com>
2025-08-28 11:54:02 -0700

    adding capacity to make it look like its walking

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -61,6 +61,18 @@ public class Player extends Entity{
 			direction = "right";
 			x += speed;
 		}
+		spriteCounter ++;
+		//gets called 60 times a second
+		//this rate will change the sprite every 10 frames
+		if(spriteCounter > 10) {
+			if(spriteNum == 1) {
+				spriteNum = 2;
+			}
+			else if(spriteNum == 2) {
+				spriteNum =1;
+			}
+			spriteCounter  = 0;
+		}
 	}
 	public void draw(Graphics2D g2) {
 		//g2.setColor(Color.white);
@@ -68,18 +80,41 @@ public class Player extends Entity{
 		BufferedImage image = null;
 		switch(direction) {
 		case"up":
-			image = up1;
+			if(spriteNum == 1) {
+				image = up1;
+			}
+			if(spriteNum == 2) {
+				image = up2;
+			}
 			break;
 		case "down" :
-			image = down1;
+			if(spriteNum == 1) {
+				image = down1;
+			}
+			if(spriteNum == 2) {
+				image = down2;
+			}
+			
 			break;
 		case "left":
-			image = left1;
+			if(spriteNum == 1) {
+				image = left1;
+			}
+			if(spriteNum == 2) {
+				image = left2;
+			}
 			break;
 		case "right":
-			image = right1;
+			if(spriteNum == 1) {
+				image = right1;
+			}
+			if(spriteNum == 2) {
+				image = right2;
+			}
 			break;
 		}
+		g2.drawImage(image, x, y,gp.TILE_SIZE,gp.TILE_SIZE,null);
+		
 		
 	}
 	
