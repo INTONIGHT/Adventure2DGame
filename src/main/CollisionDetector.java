ee3917c90c74a07cb215806957730f102e4f2fd6
INTONIGHT <tyler.throop@gmail.com>
2025-09-02 10:51:03 -0700

    taking a break for now made some good progress

--- src/main/CollisionDetector.java
+++ src/main/CollisionDetector.java
@@ -78,25 +78,45 @@ public class CollisionDetector {
 					//checks if these two rectangles touch
 					
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						
+						if(gp.obj[i].collision) {
+							entity.collisionOn = true;
+						}
+						if(player) {
+							index = i;
+						}
 					}
 					break;
 				case "down":
 					entity.solidArea.y += entity.speed;
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						
+						if(gp.obj[i].collision) {
+							entity.collisionOn = true;
+						}
+						if(player) {
+							index = i;
+						}
 					}
 					break;
 				case "left":
 					entity.solidArea.x -= entity.speed;
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						
+						if(gp.obj[i].collision) {
+							entity.collisionOn = true;
+						}
+						if(player) {
+							index = i;
+						}
 					}
 					break;
 				case "right":
 					entity.solidArea.x += entity.speed;
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
-						
+						if(gp.obj[i].collision) {
+							entity.collisionOn = true;
+						}
+						if(player) {
+							index = i;
+						}
 					}
 					break;
 				}
