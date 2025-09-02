cc48b7ff7b8ac9f3ed6b86c81b81e7c30a50e1a5
INTONIGHT <tyler.throop@gmail.com>
2025-09-02 10:06:31 -0700

    Objects are now added

--- src/main/AssetLoader.java
+++ src/main/AssetLoader.java
@@ -1,5 +1,7 @@
 package main;
 
+import object.OBJ_Chest;
+import object.OBJ_Door;
 import object.OBJ_Key;
 
 public class AssetLoader {
@@ -17,5 +19,25 @@ public class AssetLoader {
 		gp.obj[1] = new OBJ_Key();
 		gp.obj[1].worldX = 23 * gp.TILE_SIZE;
 		gp.obj[1].worldY = 40 * gp.TILE_SIZE;
+		
+		gp.obj[2] = new OBJ_Key();
+		gp.obj[2].worldX = 37 * gp.TILE_SIZE;
+		gp.obj[2].worldY = 7 * gp.TILE_SIZE;
+		
+		gp.obj[3] = new OBJ_Door();
+		gp.obj[3].worldX = 10 * gp.TILE_SIZE;
+		gp.obj[3].worldY = 11 * gp.TILE_SIZE;
+		
+		gp.obj[4] = new OBJ_Door();
+		gp.obj[4].worldX = 8 * gp.TILE_SIZE;
+		gp.obj[4].worldY = 28 * gp.TILE_SIZE;
+		
+		gp.obj[5] = new OBJ_Door();
+		gp.obj[5].worldX = 12 * gp.TILE_SIZE;
+		gp.obj[5].worldY = 22 * gp.TILE_SIZE;
+		
+		gp.obj[6] = new OBJ_Chest();
+		gp.obj[6].worldX = 10 * gp.TILE_SIZE;
+		gp.obj[6].worldY = 7 * gp.TILE_SIZE;
 	}
 }
