74d140915c4ee1574300541970a66ee54f1c01ae
INTONIGHT <tyler.throop@gmail.com>
2025-09-10 10:32:11 -0700

    making the map more interesting

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -22,26 +22,67 @@ public class TileManager {
 	
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
-		tile = new Tile[25];
+		tile = new Tile[50];
 		getTileImage();
 		//this is to generate a random tile map
 		tileMap = new Integer[gp.MAX_SCREEN_COL][gp.MAX_SCREEN_ROW];//used this for the random
 		 generateTileMap();
 		 //map from text file
 		 mapTileNum =  new int[gp.MAX_WORLD_COL][gp.MAX_WORLD_ROW];
-		 loadMap("/maps/world01.txt");
+		 loadMap("/maps/worldv3.txt");
 		
 	}
 	
 	public void getTileImage() {
-			setup(0,"grass01",false);
-			setup(1,"wall",true);
-			setup(2,"water01",true);
-			setup(3,"earth",false);
-			setup(4,"tree",true);
-			setup(5,"sand",false);
+		//PLACEHOLDER
+			setup(0,"grass00",false);
+			setup(1,"grass00",false);
+			setup(2,"grass00",false);
+			setup(3,"grass00",false);
+			setup(4,"grass00",false);
+			setup(5,"grass00",false);
+			setup(6,"grass00",false);
+			setup(7,"grass00",false);
+			setup(8,"grass00",false);
+			setup(9,"grass00",false);
+			setup(10,"grass00",false);
 			//set collision to true for tiles you want to be able to collide with remember if you want different types keep it consistent
 			//can load more images when needed
+			setup(11,"grass01",false);
+			
+			setup(12,"water00",true);
+			setup(13,"water01",true);
+			setup(14,"water02",true);
+			setup(15,"water03",true);
+			setup(16,"water04",true);
+			setup(17,"water05",true);
+			setup(18,"water06",true);
+			setup(19,"water07",true);
+			setup(20,"water08",true);
+			setup(21,"water09",true);
+			setup(22,"water10",true);
+			setup(23,"water11",true);
+			setup(24,"water12",true);
+			setup(25,"water13",true);
+			
+			setup(26,"road00",false);
+			setup(27,"road01",false);
+			setup(28,"road02",false);
+			setup(29,"road03",false);
+			setup(30,"road04",false);
+			setup(31,"road05",false);
+			setup(32,"road06",false);
+			setup(33,"road07",false);
+			setup(34,"road08",false);
+			setup(35,"road09",false);
+			setup(36,"road10",false);
+			setup(37,"road11",false);
+			setup(38,"road12",false);
+			
+			setup(39,"earth",false);
+			setup(40,"wall",true);
+			setup(41,"tree",true);
+			
 	}
 	
 	public void setup(int index, String imageName, boolean collision) {
