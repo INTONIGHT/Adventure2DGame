6817ed15fb20d797eeab7da148c986c88c5db16e
INTONIGHT <tyler.throop@gmail.com>
2025-09-29 09:47:22 -0700

    starting to rework the logic to make collisions better

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -4,19 +4,26 @@ import java.awt.Rectangle;
 
 public class EventHandler {
 	GamePanel gp;
-	Rectangle eventRect;
-	int eventRectDefaultX, eventRectDefaultY;
+	EventRect eventRect[][];
 	
 	
 	public EventHandler(GamePanel gp) {
 		this.gp = gp;
-		eventRect = new Rectangle();
-		eventRect.x = 23;
-		eventRect.y = 23;
-		eventRect.width = 2;
-		eventRect.height = 2;
-		eventRectDefaultX = eventRect.x;
-		eventRectDefaultY = eventRect.y;
+		eventRect = new EventRect[gp.MAX_WORLD_COL][gp.MAX_WORLD_ROW];
+		//allows you to have an event rect for everytile
+		int col = 0;
+		int row = 0;
+		while(col < gp.MAX_WORLD_COL && row <gp.MAX_SCREEN_ROW) {
+			eventRect[col][row] = new EventRect();
+			
+			eventRect[col][row].x = 23;
+			eventRect[col][row].y = 23;
+			eventRect[col][row].width = 2;
+			eventRect[col][row].height = 2;
+			eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+			eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+		}
+		
 		
 		
 	}
@@ -56,24 +63,24 @@ public class EventHandler {
 		gp.player.direction = "left";
 	}
 
-	public boolean hit(int eventCol, int eventRow, String reqDirection) {
+	public boolean hit(int col, int row, String reqDirection) {
 		boolean hit = false;
 		
 		gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
 		gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
 		
-		eventRect.x = eventCol*gp.TILE_SIZE + eventRect.x;
-		eventRect.y = eventRow*gp.TILE_SIZE + eventRect.y;
+		eventRect[col][row].x = col*gp.TILE_SIZE + eventRect[col][row].x;
+		eventRect[col][row].y = row*gp.TILE_SIZE + eventRect[col][row].y;
 		//System.out.println("PlayerSolidArea:" + gp.player.solidArea + " eventRect:" + eventRect);
-		if(gp.player.solidArea.intersects(eventRect)) {
+		if(gp.player.solidArea.intersects(eventRect[col][row])) {
 			if(gp.player.direction.equals(reqDirection) || reqDirection.contentEquals("any")) {
 				hit = true;
 			}
 		}
 		gp.player.solidArea.x = gp.player.solidAreaDefaultX;
 		gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-		eventRect.x = eventRectDefaultX;
-		eventRect.y = eventRectDefaultY;
+		eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+		eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 		
 		return hit;
 	}
